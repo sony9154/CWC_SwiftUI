14a86fc5ba537e1013ac590d5635c38e8fd634f3
sony9154 <emus-04reunion@icloud.com>
2023-12-08 01:42:48 +0800

    Finish L3

--- L3 Demo 2/L3 Demo 2/ContentView.swift
+++ L3 Demo 2/L3 Demo 2/ContentView.swift
@@ -10,19 +10,61 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        VStack(alignment: .leading, spacing: 20.0) {
-            Image("niagarafalls")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .cornerRadius(15)
+        ZStack {
+            Color(.systemMint)
+                .ignoresSafeArea()
             
-            Text("Niagara Falls")
-                .font(.title)
-                .fontWeight(.bold)
+            VStack(alignment: .leading, spacing: 20.0) {
+                Image("niagarafalls")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
                 
-            Text("Come visit the falls for an experience of a lifetime.")
-            
+                HStack {
+                    
+                    
+                    Text("Niagara Falls")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        HStack {
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.filled")
+                        }
+                        Text("(Review 361)")
+                    }
+                    .foregroundColor(.orange)
+                    .font(.caption)
+                    
+                    
+                }
+                
+                Text("Come visit the falls for an experience of a lifetime.")
+                
+                HStack {
+                    Spacer()
+                    Image(systemName: "fork.knife")
+                    Image(systemName: "binoculars.fill")
+                }
+                .foregroundColor(.gray)
+                .font(.caption)
+                
+                
+            }
+            .padding()
+            .background(Rectangle()
+                .foregroundColor(.white))
+                .cornerRadius(15)
+                .shadow(radius: 15)
+            .padding()
         }
+        
         
         
         

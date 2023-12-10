018f61eb4379f93f1f96a089a2238ed8939431e6
sony9154 <emus-04reunion@icloud.com>
2023-12-10 17:20:09 +0800

    Finish War Car Game UI

---
+++ War Card Game/War Card Game/ContentView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  War Card Game
+//
+//  Created by Hsu Hua on 2023/12/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        ZStack {
+            Image("background-plain")
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image("card2")
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                }
+                Spacer()
+                Image("button")
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                .foregroundColor(Color.white)
+                Spacer()
+                
+            }
+        }
+        
+        
+        
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ War Card Game/War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Hsu Hua on 2023/12/8.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

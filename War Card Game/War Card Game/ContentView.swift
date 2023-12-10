6e091c0e9f4465b4906b8eb81925fa4875dea4cb
sony9154 <emus-04reunion@icloud.com>
2023-12-11 01:14:01 +0800

    Finish War Card Game

--- War Card Game/War Card Game/ContentView.swift
+++ War Card Game/War Card Game/ContentView.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var playerCard = "card7"
-    var cpuCard = "card13"
+    @State var playerCard = "card7"
+    @State var cpuCard = "card13"
     
-    var playerScore = 0
-    var cpuScore = 0
+    @State var playerScore = 0
+    @State var cpuScore = 0
     
     var body: some View {
         
@@ -72,7 +72,23 @@ struct ContentView: View {
     }
     
     func deal() {
-        print("Deal cards")
+        // Randomize the players card
+        let playerCardValue = Int.random(in: 2...14)
+        playerCard = "card" + String(playerCardValue)
+        
+        // Randomize the cpus card
+        let cpuCardValue = Int.random(in: 2...14)
+        cpuCard = "card" + String(cpuCardValue)
+        
+        // Update the scores
+        if playerCardValue > cpuCardValue {
+            // Add 1 to player score
+            playerScore += 1
+        }
+        else if cpuCardValue > playerCardValue {
+            // Add 1 to cpu score
+            cpuScore += 1
+        }
     }
 }
 
